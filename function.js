9afc22b7f54fccf55a1f781d92da656d825a9ab5
samia <samianishatox@gmail.com>
2024-10-16 11:56:00 +0600

    task 1

---
+++ function.js
@@ -0,0 +1,9 @@
+// Task-1
+// Take four parameters. Multiply the four numbers and then return the result
+
+function multiply (num1, num2, num3, num4){
+    const product = num1*num2*num3*num4;
+    return product;
+}
+const product = multiply(12,13,24,25);
+console.log(product);
