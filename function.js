776fb9291cc2d10a039e29c1d555867737515fd2
samia <samianishatox@gmail.com>
2024-10-16 19:58:36 +0600

    Solve Task 5

--- function.js
+++ function.js
@@ -40,3 +40,40 @@ function make_avg(integers) {
 }
 const arrayOfIntegers = [12, 13, 15, 21, 93];
 console.log(make_avg(arrayOfIntegers));
+
+
+// Task-4
+// Write a function called count_zero() which will take a binary string
+//  (Binary string is a string which is consist of only 0 and 1)
+//   as parameter and count how many 0’s are there in that string.
+let zeroArray = [];
+function count_zero(binaryStr){
+    console.log(binaryStr);
+    for (let i = 0; i < binaryStr.length; i++  ){
+        if(binaryStr[i] === '0'){
+            zeroArray.push('0');
+        }
+    }
+    // return zeroArray;
+}
+const binary = '01010110011010110';
+// zeroArray = 
+count_zero(binary);
+
+console.log(zeroArray);
+console.log(zeroArray.length);
+
+
+// Task-5
+// Write a function called odd_even() which takes an integer value and tells whether this value is even or odd.
+//  If even return Even. If odd return Odd
+
+function odd_even(integer){
+    if(integer % 2 === 0){
+        console.log('Even');
+    }
+    else{
+      console.log('Odd');  
+    }
+}
+odd_even(123);
