6ded57fea63c31abd58961efe4686a7568fe97b0
samia <samianishatox@gmail.com>
2024-10-16 12:28:50 +0600

    Solve Task 3

--- function.js
+++ function.js
@@ -12,14 +12,31 @@ console.log(product);
 // Take a number if the number is odd multiply it by 2 and return the result. 
 // If the number is even divide it by two and return the result.
 
-function evenOrOdd (number){
-    if(number %2===1){
-        const multiply = number*2;
+function evenOrOdd(number) {
+    if (number % 2 === 1) {
+        const multiply = number * 2;
         return multiply;
     }
-    else{
-        const divide = number/2;
+    else {
+        const divide = number / 2;
         return divide;
     }
 }
 console.log(evenOrOdd(9));
+
+// Task - 3
+// Write a function called make_avg() which will take an array of integers and the size of that array 
+// and return the average of those values.
+let sum;
+function make_avg(integers) {
+    const size = integers.length;
+    sum = size;
+    for (number of integers) {
+        sum = sum + number;
+    }
+    const divisor = size + 1;
+    const average = sum / divisor;
+    return average;
+}
+const arrayOfIntegers = [12, 13, 15, 21, 93];
+console.log(make_avg(arrayOfIntegers));
