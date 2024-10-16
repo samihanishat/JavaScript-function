11f2fb9e0ad765a32e818f3fffd2b1ff4b536f9d
samia <samianishatox@gmail.com>
2024-10-16 12:04:39 +0600

    Solve task 2

--- function.js
+++ function.js
@@ -1,9 +1,25 @@
 // Task-1
 // Take four parameters. Multiply the four numbers and then return the result
 
-function multiply (num1, num2, num3, num4){
-    const product = num1*num2*num3*num4;
+function multiply(num1, num2, num3, num4) {
+    const product = num1 * num2 * num3 * num4;
     return product;
 }
-const product = multiply(12,13,24,25);
+const product = multiply(12, 13, 24, 25);
 console.log(product);
+
+// Task-2
+// Take a number if the number is odd multiply it by 2 and return the result. 
+// If the number is even divide it by two and return the result.
+
+function evenOrOdd (number){
+    if(number %2===1){
+        const multiply = number*2;
+        return multiply;
+    }
+    else{
+        const divide = number/2;
+        return divide;
+    }
+}
+console.log(evenOrOdd(9));
